e5112c9ad4abe6655b75c11cfb9dc8f48c5200fd
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2024-10-07 21:58:43 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,8 @@
 let getSettings = () => {
     return {
         // api:"http://localhost:3004"
-         api : "https://airbnb-clone-by-jay.herokuapp.com"
+         // api : "https://airbnb-clone-by-jay.herokuapp.com"
+         api : "https://capstone-project-backend-sigma.vercel.app"
         
     }
 }
